313a03d238eea1222bd3bfd823bb5ab802ab5701
m-cvvirko <marek@cvvirko.pl>
2024-11-21 23:23:14 +0100

    added account repository

---
+++ app/src/main/java/org/example/model/Account.java
@@ -0,0 +1,36 @@
+package org.example.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String surname;
+
+    private Double balancePLN;
+
+    private String accountIdentifier;
+
+    @Transient
+    private Double balanceUSD;
+}
